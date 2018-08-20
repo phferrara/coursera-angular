8ef08c3c8a876e0f8d9425a553492a67caf84d6f
Philippe Ferrara <philippe.ferrara@gmx.ch>
2018-08-20 23:28:44 +0200

    Basic Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import 'hammerjs';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatToolbarModule,
     FlexLayoutModule
   ],
-  providers: [],
+  providers: [ DishService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
