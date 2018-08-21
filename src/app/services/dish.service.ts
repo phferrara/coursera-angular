cd944182e10c81ac83fbcd83e605b6803e190feb
Philippe Ferrara <philippe.ferrara@gmx.ch>
2018-08-21 02:24:19 +0200

    SPA Part 1

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -12,4 +12,12 @@ export class DishService {
   getDishes(): Dish[] {
     return DISHES;
   }
+
+  getDish(id: number) {
+    return DISHES.filter((dish) => (dish.id === id))[0];
+  }
+
+  getFeaturedDish() {
+    return DISHES.filter((dish) => (dish.featured))[0];
+  }
 }
